da981f04c857cbb747ccabc6e4da47946db6c672
AquaUseful <l.vladik.m@gmail.com>
2022-05-04 00:19:49 +0300

    Add GTK drawing stuff

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -3,6 +3,12 @@
 #include <iostream>
 #include <ostream>
 
+#include <gtkmm/drawingarea.h>
+#include <gtkmm/application.h>
+#include <gtkmm/window.h>
+#include <cairomm/context.h>
+
+#include "ShapeWindow.hpp"
 #include "Vec.hpp"
 #include "Rhombus.hpp"
 #include "Rectange.hpp"
@@ -10,11 +16,62 @@
 
 const Logger::Level level = Logger::Level::Info;
 
-int main() {
+class MyArea : public Gtk::DrawingArea
+{
+public:
+  MyArea();
+  virtual ~MyArea();
 
-  Logger::instance().loglevel() = level;
+protected:
+  void on_draw(const Cairo::RefPtr<Cairo::Context>& cr, int width, int height);
+};
 
-  shapes::Rhombus<int> r(shapes::Vec2D<int>(10, 5), 5);
+class ExampleWindow : public Gtk::Window
+{
+public:
+  ExampleWindow();
 
-  return EXIT_SUCCESS;
+protected:
+  MyArea m_area;
+};
+
+MyArea::MyArea()
+{
+  set_draw_func(sigc::mem_fun(*this, &MyArea::on_draw));
+}
+
+MyArea::~MyArea()
+{
+}
+
+void MyArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr, int width, int height)
+{
+  // coordinates for the center of the window
+  int xc, yc;
+  xc = width / 2;
+  yc = height / 2;
+
+  cr->set_line_width(10.0);
+
+  // draw red lines out from the center of the window
+  cr->set_source_rgb(0.0, 0.0, 0.0);
+  cr->move_to(0, 0);
+  cr->line_to(xc, yc);
+  cr->line_to(0, height);
+  cr->move_to(xc, yc);
+  cr->line_to(width, yc);
+  cr->stroke();
+}
+
+ExampleWindow::ExampleWindow()
+{
+  set_title("DrawingArea");
+  set_child(m_area);
+}
+
+int main(int argc, char** argv)
+{
+  auto app = Gtk::Application::create("org.gtkmm.example");
+
+  return app->make_window_and_run<draw::ShapeWindow>(argc, argv);
 }
